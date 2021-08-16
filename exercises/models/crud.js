d102177b53aecd419a553130358e77db7fdb16fd
shubham <shubhamdv19@gmail.com>
2021-08-16 16:52:21 +0530

    passed all the tests

--- exercises/models/crud.js
+++ exercises/models/crud.js
@@ -1,22 +1,23 @@
+const user = require('./user')
 const User = require('./user')
 
 const getUserById = (id) => {
- 
+  return User.findById(id).exec()
 }
 
 const getAllUsers = () => {
-  
+  return User.find({}).exec()
 }
 
 const createUser = (userDetails) => {
-  
+  return User.create(userDetails)
 }
 const removeUserById = (id) => {
-  
+  return User.findByIdAndRemove(id).exec();
 }
 
 const updateUserById = (id, update) => {
-
+  return User.findByIdAndUpdate(id, update, {new: true})
 }
 
 module.exports = {
